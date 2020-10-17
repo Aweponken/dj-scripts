b2a51ded3472353cdf90caf5b3832e8480f67445
Arvid Widerberg <ssh@awide.se>
2020-10-17 21:07:58 +0200

    Add file removal script

---
+++ remove_files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script removes files that matches an expression, such as *.mp3
+# Usage: ./remove_files.sh <search_folder> <expression>
+# The folder name may be absolute or relative.
+
+# If less than two arguments supplied, display usage message
+if [  $# -lt 2 ]; then 
+  echo -e "\nUsage: ./remove_files.sh <search_folder> <expression> \n" 
+  exit 1
+fi 
+
+
+# Sanitize any previous escaping from the folder argument
+SEARCH_FOLDER=$(echo "${1//\\}")
+EXPRESSION=$2
+
+
+echo "Searching folder $SEARCH_FOLDER for files matching $EXPRESSION"
+find $SEARCH_FOLDER -name $EXPRESSION -print0 | while read -d $'\0' file
+do
+  echo "Removing $file"
+  if ! rm "$file"; then
+    echo -e "\nRemoval failed!\n"
+    exit 1
+  fi
+done
+
+echo -e "\nRemoval completed!\n"
